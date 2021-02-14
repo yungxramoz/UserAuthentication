03d89d59fe9217473bd950bdd44ac01c686e8489
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-14 16:21:05 +0100

    Add InfoException and PasswordHelper

---
+++ UserAuthentication/Helpers/InfoException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace UserAuthentication.Helpers
+{
+    public class InfoException: Exception
+    {
+        public InfoException() : base() { }
+
+        public InfoException(string message) : base(message) { }
+
+        public InfoException(string message, params object[] args)
+            : base(String.Format(CultureInfo.CurrentCulture, message, args))
+        {
+        }
+    }
+}
