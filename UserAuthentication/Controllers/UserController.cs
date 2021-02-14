d9b7ee87bb6af0ad1b2a1d91d02c8f0dab4343f4
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-14 20:07:55 +0100

    Add User Controller

---
+++ UserAuthentication/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using UserAuthentication.Data.Entities;
+using UserAuthentication.Helpers;
+using UserAuthentication.Models;
+using UserAuthentication.Services;
+
+namespace UserAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+        private IMapper _mapper;
+        private readonly AppSettings _appSettings;
+
+        public UserController(IUserService userService, IMapper mapper, IOptions<AppSettings> appSettings)
+        {
+            _userService = userService;
+            _mapper = mapper;
+            _appSettings = appSettings.Value;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = _userService.Get();
+            var model = _mapper.Map<IList<UserModel>>(users);
+            return Ok(model);
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _userService.Get(id);
+            var model = _mapper.Map<UserModel>(user);
+            return Ok(model);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] RegistrationModel model)
+        {
+            var user = _mapper.Map<User>(model);
+
+            try
+            {
+                _userService.Create(user, model.Password);
+                return Ok();
+            }
+            catch (InfoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateModel model)
+        {
+            var user = _mapper.Map<User>(model);
+            user.PersonId = id;
+
+            try
+            {
+                _userService.Update(user, model.Password);
+                return Ok();
+            }
+            catch (InfoException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userService.Delete(id);
+            return Ok();
+        }
+    }
+}
--- UserAuthentication/Startup.cs
+++ UserAuthentication/Startup.cs
@@ -1,20 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using UserAuthentication.Data;
-using AutoMapper;
 using UserAuthentication.Helpers;
 using Microsoft.EntityFrameworkCore;
+using UserAuthentication.Services;
 
 namespace UserAuthentication
 {
@@ -39,6 +33,10 @@ namespace UserAuthentication
             var appSettingsSection = _configuration.GetSection("AppSettings");
             services.Configure<AppSettings>(appSettingsSection);
 
+            //DI services
+            services.AddScoped<IUserService, UserService>();
+
+            //Swagger stuff
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserAuthentication", Version = "v1" });
@@ -57,7 +55,8 @@ namespace UserAuthentication
 
             userContext.Database.Migrate();
 
-            app.UseHttpsRedirection();
+            //TODO handle HTTPS
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
