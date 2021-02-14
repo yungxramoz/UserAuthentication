2fff258b368ca18d2bb3684a7c008d19d634bcb6
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-14 16:21:24 +0100

    Add IUserService Interface

---
+++ UserAuthentication/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UserAuthentication.Data.Entities;
+
+namespace UserAuthentication.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> Get();
+        User Get(int id);
+        User Create(User user, string password);
+        void Update(User user, string password);
+        void Delete(int id);
+    }
+}
