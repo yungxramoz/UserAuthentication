e76e93e27c692826dc016a37bc4e885dc111dce0
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-19 16:30:44 +0100

    Add authorization to swagger doc

--- UserAuthentication/Startup.cs
+++ UserAuthentication/Startup.cs
@@ -16,6 +16,7 @@ using UserAuthentication.Services;
 using UserAuthentication.Data.Entities;
 using System.Reflection;
 using System.IO;
+using System.Collections.Generic;
 
 namespace UserAuthentication
 {
@@ -96,6 +97,36 @@ namespace UserAuthentication
                     {
                         Name = "MIT",
                         Url = new Uri("https://opensource.org/licenses/MIT")
+                    }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                        {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                            {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
                     }
                 });
 
