d96e13e7f2fac78ed24499b1665c403858edde13
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-19 16:50:13 +0100

    Fix format issue in auth dialog on swagger doc

--- UserAuthentication/Startup.cs
+++ UserAuthentication/Startup.cs
@@ -102,9 +102,9 @@ namespace UserAuthentication
 
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
-                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Description = @"JWT Authorization header using the Bearer scheme. <br />
+                      Enter 'Bearer' [space] and then your token in the text input below. <br /> <br /> 
+                      <strong>Example: 'Bearer 12345abcdef'</strong><br /><br /> ",
                     Name = "Authorization",
                     In = ParameterLocation.Header,
                     Type = SecuritySchemeType.ApiKey,
@@ -113,13 +113,13 @@ namespace UserAuthentication
 
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement()
                 {
-                        {
+                    {
                         new OpenApiSecurityScheme
                         {
                         Reference = new OpenApiReference
                             {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
                             },
                             Scheme = "oauth2",
                             Name = "Bearer",
